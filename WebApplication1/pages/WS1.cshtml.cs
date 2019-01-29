927b1421611fa3833857704a59521c8a47648f27
arodonnell <arodonnell@outlook.com>
2019-01-29 23:01:18 +0000

    Update

--- WebApplication1/pages/WS1.cshtml.cs
+++ WebApplication1/pages/WS1.cshtml.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication1.Pages
 {
-    public class ws1Model : PageModel
+    public class WS1Model : PageModel
     {
         public string[] MyTestArray { get; set; }
         = { "Ireland", "Scotland", "Wales", "England" };
 
-        public string Message { get; set; }
+        public string Message1 { get; set; }
+        public string Message2 { get; set; }
         public object Name { get; set; }
         public int Number1 { get; set; }
         public int Number2 { get; set; }
@@ -23,10 +24,10 @@ namespace WebApplication1.Pages
         }
         public void OnPost()
         {
-            Message =
+            Message1 =
                      String.Format("Hello  {0}", Name);
 
-            Message =
+            Message2 =
                 String.Format("The Sum of {0} and {1} is {2}", Number1, Number2, (Number1 + Number2));
         }
         
