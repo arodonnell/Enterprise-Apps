59d42d687063e21e673afe3443752f1101877a06
Adrian O Donnell <S00190500@mail.itsligo.ie>
2019-01-29 09:48:48 +0000

    Setup

---
+++ WebApplication1/pages/WS1.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class WS1Model : PageModel
+    {
+        public string[] MyTestArray { get; set; }
+        = { "Ireland", "Scotland", "Wales", "England" };
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            @Model.Message = "Hello {0}", name;
+        }
+    }
+}
